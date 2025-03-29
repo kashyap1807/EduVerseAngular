616e6d2ec8a4a5da40164683a0a19bde8d924d90
Kashyap <kashyapchudasama0@gmail.com>
2025-03-29 18:21:32 +0530

    created navbar, update home page and carousel, add plan-pricisng page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { PlansAndPricingComponent } from "../plans-and-pricing/plans-and-pricing.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule, PlansAndPricingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
