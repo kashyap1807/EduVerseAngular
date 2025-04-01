7bbb4b033d8b3667a6dcd4852b842744e1f50780
Kashyap <kashyapchudasama0@gmail.com>
2025-04-01 19:31:04 +0530

    put ngx spinner and toastr for testing purpose

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,34 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ToastrModule } from 'ngx-toastr';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpRequestInterceptor } from './services/interceptor/spinner-interceptor';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(CarouselModule.forRoot()),
+    importProvidersFrom(CarouselModule.forRoot(),
+      BrowserModule,
+      BrowserAnimationsModule,
+      NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
+      ToastrModule.forRoot({
+        timeOut: 3000,
+        positionClass: 'toast-top-right',
+        preventDuplicates: true,
+      }),
+    ),
     provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
+      multi: true,
+    },
   ],
 };
---
+++ src/app/services/interceptor/spinner-interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { catchError, finalize, map, Observable, throwError } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class HttpRequestInterceptor implements HttpInterceptor {
+
+    constructor(private spinner:NgxSpinnerService,private toastr:ToastrService) { }
+    // This method intercepts HTTP requests and allows you to modify them before they are sent.
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        this.spinner.show(); // Show the spinner when a request is made
+        return next.handle(req).pipe(
+            map((event: HttpEvent<any>) => {
+                if (event instanceof HttpResponse) {
+                    this.toastr.success('Request completed successfully!'); // Show success message on response
+                }
+                return event;
+            }),
+            catchError((error: HttpErrorResponse) => {
+                this.toastr.error(`Request failed: ${error.message}`); 
+                return throwError(error);
+            }),
+            finalize(() => {
+                this.spinner.hide(); // Hide the spinner when the request is completed
+            })
+        );
+    }
+}
