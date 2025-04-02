65dd6fbaafde325432d99f2e2d6a6227a9940476
Kashyap <kashyapchudasama0@gmail.com>
2025-04-02 09:13:12 +0530

    partially Contact-us  created

--- src/app/components/core/contact-us/contact-us.component.ts
+++ src/app/components/core/contact-us/contact-us.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ContactService } from '../../../services/contact.service';
+import { ToastrService } from 'ngx-toastr';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule,ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrl: './contact-us.component.css',
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
+  contactForm!: FormGroup;
+  userId = 0;
+  constructor(
+    private fb: FormBuilder,
+    private contactService: ContactService,
+    private toastrService: ToastrService,
+    // private loginService: LoginService,
+    // private userService: UserProfileService
+  ) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
 
+  ngOnInit(): void {
+    this.triggerError();
+    // this.userId = this.loginService.userId;
+  }
+
+  triggerError() {
+    // Simulate a non-HTTP error
+    throw new Error('This is a simulated error');
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.contactService.sendMessage(this.contactForm.value).subscribe(
+        (response) => {
+          this.toastrService.success('Message sent successfully!');
+          this.contactForm.reset(); // Reset form after submission
+        },
+        (error) => {
+          this.toastrService.error('Error sending message. Please try again.');
+        }
+      );
+    }
+  }
 }
