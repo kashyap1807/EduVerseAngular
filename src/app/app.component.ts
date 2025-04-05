780bf2cfd8d48658884333a3128ba8bb32b7cbaf
Kashyap <kashyapchudasama0@gmail.com>
2025-04-06 00:45:10 +0530

    Footer Component Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { NavBarComponent } from "./components/core/nav-bar/nav-bar.component";
 import { NgxSpinner, NgxSpinnerComponent } from 'ngx-spinner';
+import { FooterComponent } from "./components/core/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent,NgxSpinnerComponent],
+  imports: [RouterOutlet, NavBarComponent, NgxSpinnerComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
