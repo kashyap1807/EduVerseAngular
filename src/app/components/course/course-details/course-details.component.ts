0129fae0904311b4b441784b9793b11dbbc05c35
Kashyap <kashyapchudasama0@gmail.com>
2025-04-15 20:08:24 +0530

    In Course details Get VideosURL

--- src/app/components/course/course-details/course-details.component.ts
+++ src/app/components/course/course-details/course-details.component.ts
@@ -4,17 +4,19 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CourseService } from '../../../services/course.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { SafePipe } from "../../../pipes/safe.pipe";
 
 @Component({
   selector: 'app-course-details',
   standalone: true,
-  imports: [CommonModule,FormsModule,RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, SafePipe],
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.css',
 })
 export class CourseDetailsComponent implements OnInit {
   courseDetails: CourseDetail | null = null;
   courseId: number = 0;
+  videoUrl: string | null = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,13 +26,26 @@ export class CourseDetailsComponent implements OnInit {
     this.route.paramMap.subscribe((params) => {
       this.courseId = Number(params.get('courseId'));
     });
-    this.getCourseDetails();  
+    this.getCourseDetails();
   }
 
   getCourseDetails() {
     this.courseService.getCourseDetails(this.courseId).subscribe((data) => {
-     this.courseDetails = data;
+      this.courseDetails = data;
     });
   }
-  
+  openVideo(videoUrl: string): void {
+    const videoId = this.extractVideoId(videoUrl);
+    this.videoUrl = `https://www.youtube.com/embed/${videoId}`;
+  }
+
+  closeVideo(): void {
+    this.videoUrl = null;
+  }
+
+  private extractVideoId(url: string): string {
+    const regex = /youtube\.com\/watch\?v=([^"&?/]{11})/;
+    const matches = url.match(regex);
+    return matches ? matches[1] : '';
+  }
 }
